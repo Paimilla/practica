630a4236b1f6780cf253c45a77d6f6d24b95917e
Gustavo <gustavocars@gmail.com>
2022-09-27 00:29:49 -0300

    PHP y HTML con tablas.

---
+++ Gustavo/index_tables.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head>
+  <body>
+  <?php
+      $fecha= date('d-m-Y');
+      $hora = date('H:i:s');
+      $hora = str_replace(":", "-", $hora);
+
+      $nombre = $_POST['nombre'];
+      $apellido = $_POST['apellido'];
+      $rut = $_POST['rut'];
+      $correo = $_POST['correo'];
+
+      $nombreCarpeta = $nombre . "_" . $apellido; // El nombre de la carpeta tendra el siguiente formato: Nombre_Apellido.
+      $nombreArchivo = $nombre . "_" . $apellido . "_" . $fecha . "_" . $hora; // El nombre de archivo tendra el siguiente formato: Nombre_Apellido_Fecha_Hora.
+      $ruta = "$nombreCarpeta/$nombreArchivo.txt"; // Ruta donde se creará el archivo y su extensión.
+
+      $mensaje = "Hola $nombre!\nTu nombre es: $nombre\nTu apellido es: $apellido\nTu RUT es: $rut\nTu correo es: $correo";
+
+      if (file_exists("$nombreCarpeta") == true) {
+          /* Si el archivo (carpeta) exsite, entregamos
+              un mensaje especificando que la ruta ya existe.
+          */
+          echo "<p>El directorio YA existe!</p><br>$ruta";
+      } else {
+          /* Caso contrario, la carpeta será creada con el nombre
+              definido en la variable $nombreCarpeta
+          */
+
+          echo "<h4>$nombre!</h4><br><p>Tu directorio personal ha sido creado.</p>";
+
+          mkdir("$nombreCarpeta");
+
+          // Escritura dentro del archivo.
+          $archivo = fopen($ruta, "w+") or die ("El archivo no se puede leer.");
+          fputs($archivo, $mensaje);
+          fclose($archivo);
+      }
+      ?>
+
+        <table>
+            <tr>
+                <td>
+                    <h1>
+                        Datos de:
+                        <?php echo "$nombre $apellido"; ?>
+                    </h1>
+                </td>
+            </tr>
+            <tr>
+              <td width=50%><b>RUT</b></td>
+              <td width=50%><b>Correo</b></td>
+            </tr>
+            <tr>
+              <td width=50%><?php echo "$rut"; ?> </td>
+              <td width=50%><?php echo "$correo"; ?> </td>
+            </tr>
+            <tr>
+              <td width=50%><b>Nombre</b></td>
+              <td width=50%><b>Apellido</b></td>
+            </tr>
+            <tr>
+              <td width=50%><?php echo "$nombre"; ?> </td>
+              <td width=50%><?php echo "$apellido"; ?> </td>
+            </tr>
+        </table>
+  </body>
+</html>
